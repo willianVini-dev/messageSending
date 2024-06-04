90e5dabb845cd5d72c7f6a0ba23cb369471432c1
Willian vinicius <vini-np7@hotmail.com>
2024-06-04 15:37:36 -0300

    create route message

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './');
+    cb(null, './'); 
   },
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + path.extname(file.originalname));
+    cb(null, file.fieldname + req.params.session + path.extname(file.originalname));
   },
 });
 
@@ -19,6 +20,8 @@ const upload = multer({ storage: storage });
 
 const app = express();
 const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 let clients = {};
 
@@ -47,35 +50,8 @@ async function createClient(sessionName, res) {
   );
 }
 
-// Rota para obter o QR Code como imagem para múltiplos clientes
-app.get('/qrcode/:session', async (req, res) => {
-  const sessionName = req.params.session;
-  try {
-    clients[sessionName] = await createClient(sessionName, res);
-    return
-    //res.send(`QR Code gerado para sessão ${sessionName}. Verifique os logs do servidor para escanear o QR Code.`);
-
-  } catch (error) {
-    console.error(`Erro ao criar o cliente Venom para a sessão ${sessionName}:`, error);
-    res.status(500).send(`Erro ao criar o cliente Venom para a sessão ${sessionName}.`);
-  }
-});
-
-// Rota para checar se já está conectado
-app.get('/connected/:session', async (req, res) => {
-  const sessionName = req.params.session;
-  try {
-    const client = clients[sessionName];
-    const isConnected = client ? await client.isConnected() : false;
-    res.json({ connected: isConnected });
-  } catch (error) {
-    console.error(`Erro ao verificar estado de conexão da sessão ${sessionName}:`, error);
-    res.status(500).send(`Erro ao verificar estado de conexão da sessão ${sessionName}.`);
-  }
-});
-
 // Função para iniciar o processo de envio de mensagens via WhatsApp
-async function startWhatsAppProcess(sessionName, message, filename, image,res) {
+async function startWhatsAppProcess(sessionName, message, image,res) {
   try {
     const client = clients[sessionName];
 
@@ -94,7 +70,7 @@ async function startWhatsAppProcess(sessionName, message, filename, image,res) {
         });
     }
 
-    const filePath = path.join(__dirname, filename);
+    const filePath = path.join(__dirname, `file${sessionName}.csv`);
     const workbook = XLSX.readFile(filePath);
     const sheet_name_list = workbook.SheetNames;
     const worksheet = workbook.Sheets[sheet_name_list[0]];
@@ -130,19 +106,60 @@ async function startWhatsAppProcess(sessionName, message, filename, image,res) {
   }
 }
 
-// Rota POST que recebe um arquivo XLSX e uma mensagem
+// Rota para obter o QR Code como imagem para múltiplos clientes
+app.get('/qrcode/:session', async (req, res) => {
+  const sessionName = req.params.session;
+  try {
+    if(clients[sessionName]){
+      res.json({ connected: true });
+      return
+    }
+
+    clients[sessionName] = await createClient(sessionName, res);
+    return
+    //res.send(`QR Code gerado para sessão ${sessionName}. Verifique os logs do servidor para escanear o QR Code.`);
+
+  } catch (error) {
+    console.error(`Erro ao criar o cliente Venom para a sessão ${sessionName}:`, error);
+    res.status(500).send(`Erro ao criar o cliente Venom para a sessão ${sessionName}.`);
+  }
+});
+
+// Rota para checar se já está conectado
+app.get('/connected/:session', async (req, res) => {
+  const sessionName = req.params.session;
+  try {
+    const client = clients[sessionName];
+    const isConnected = client ? await client.isConnected() : false;
+    res.json({ connected: isConnected });
+  } catch (error) {
+    console.error(`Erro ao verificar estado de conexão da sessão ${sessionName}:`, error);
+    res.status(500).send(`Erro ao verificar estado de conexão da sessão ${sessionName}.`);
+  }
+});
+
+
+// Rota POST que recebe um arquivo XLSX
 app.post('/upload/:session', upload.single('file'), (req, res) => {
+  const file = req.file;
+
+  if (!file) {
+    return res.status(400).send('Por favor, forneça um arquivo CSV.');
+  }
+
+  res.json({message:true})
+});
+
+// Rota POST que recebe uma mensagem
+app.post('/message/:session', (req, res) => {
   const sessionName = req.params.session;
   const message = req.body.message;
   const image = req.body.image;
-  const file = req.file;
-
-  if (!message || !file) {
-    return res.status(400).send('Por favor, forneça um arquivo CSV e uma mensagem.');
+  if (!message) {
+    return res.status(400).send('Por favor, forneça uma mensagem.');
   }
-
   // Iniciar o processo de enviar mensagens após o upload do arquivo
-  startWhatsAppProcess(sessionName, message, req.file.filename,image, res);
+  startWhatsAppProcess(sessionName, message,image,res);
 });
 
 // Iniciando o servidor
